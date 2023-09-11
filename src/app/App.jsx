6ebf31608d3ff9a90a0d3d3da9a905c0308e5006
vagabund3219 <vagabund3219@gmail.com>
2023-09-12 00:59:24 +0300

    [UPDATE]: Global navbar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
 import MainPage from '../pages/MainPage'
+import CartPage from '../pages/CartPage'
+import Navbar from '../components/Navbar'
 
 function App() {
 
   return (
     <>
        <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<MainPage/>}/>
+          <Route path='/cart' element={<CartPage/>}/>
         </Routes>
        </BrowserRouter> 
     </>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
+
 function MainPage() {
   return (
     <div>
-        MainPage
     </div>
   )
 }
