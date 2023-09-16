45c882775a3c3505416b8eb5ba378ce3b350b1e2
vagabund3219 <vagabund3219@gmail.com>
2023-09-16 17:28:15 +0300

    [ADD]: Spiner for loading

---
+++ src/components/Loader.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Spinner = styled.div` 
+  border: 16px solid #d2cbcb; 
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function Loader() {
+  return (
+    <Container>
+        <Spinner/>
+    </Container>
+  )
+}
+
+export default Loader
