3c3bfc6956db651343f0d6c92a9a112500065cac
vagabund3219 <vagabund3219@gmail.com>
2023-09-19 21:09:39 +0300

    [ADD]: Added modal window

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,46 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+
 import MainPage from '../pages/MainPage'
 import CartPage from '../pages/CartPage'
 import Navbar from '../components/Navbar'
 
+import Modal from '../features/modal/Modal'
+import Login from '../features/auth/Login'
+import { modalSlice } from '../features/modal/modalSlice'
+import Categories from '../features/categories/Categories'
+
+import { checkAuth } from './actionCreators'
+
+import './App.scss'
+import Register from '../features/auth/Register'
+
+
 function App() {
+  const [showRegister, setShowRegister] = useState(false);
+  const { isOpen } = useSelector(state => state.modal)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+        dispatch(checkAuth())
+    }
+  }, [])
 
   return (
     <>
        <BrowserRouter>
+        {isOpen && <Modal
+            children={showRegister === true ? 
+              <Register setShowRegister={setShowRegister}/>
+              : 
+              <Login setShowRegister={setShowRegister}/>}
+            onClose={() => dispatch(modalSlice.actions.close())}
+        />}
         <Navbar/>
         <Routes>
+          <Route path='/' element={<Categories/>}/>
           <Route path='/catalog' element={<MainPage/>}/>
           <Route path='/cart' element={<CartPage/>}/>
         </Routes>
---
+++ src/features/modal/Modal.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const ModalContainer = styled.div`
+  position: relative;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+`;
+
+const ModalSpan = styled.button`
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  transition: all .3s ease-in;
+  transform: scale(1.1);
+
+  &:hover {
+    color: red;
+    transform: scale(1.2);
+  }
+`;
+
+const Modal = ({ children, onClose }) => {
+  return (
+    <ModalOverlay>
+      <ModalContainer>
+        {children}
+        <ModalSpan onClick={onClose}>&#10006;</ModalSpan>
+      </ModalContainer>
+    </ModalOverlay>
+  );
+};
+
+export default Modal;
