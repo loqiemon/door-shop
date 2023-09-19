72f8b61a60ceccb38a6affc3e5fcff961a2fe460
vagabund3219 <vagabund3219@gmail.com>
2023-09-19 21:11:25 +0300

    [ADD]: Login and Register components with logic

--- src/app/actionCreators.js
+++ src/app/actionCreators.js
@@ -2,16 +2,72 @@ import axios from "axios";
 import { cartSlice } from '../features/cart/cartSlice';
 import { categoriesSlice } from '../features/categories/categoriesSlice';
 import { API_URL } from "../services/constants";
+import { authSlice } from "../features/auth/authSlice";
+import AuthService from "../services/authService";
+import { modalSlice } from "../features/modal/modalSlice";
 
 
 export const fetchCategories = () => async(dispatch) => {
     try {
         dispatch(categoriesSlice.actions.categoriesFetching())
-        const response = await axios.get(API_URL+"/categories");
+        const response = await axios.get(API_URL+"categories");
         dispatch(categoriesSlice.actions.categoriesFetchingSuccess(response.data))
     } catch (e) {
         if (e) {
             dispatch(categoriesSlice.actions.categoriesFetchingError(e.message))
+        }
+    }
+}
+
+
+export const loginFunc = (login, password) => async(dispatch) => {
+    try {
+        dispatch(authSlice.actions.login());
+        // const response = await axios.post(API_URL+"login", {login, password});
+        const response = await AuthService.login(login, password);
+        localStorage.setItem('token', response.data.accessToken);
+        dispatch(authSlice.actions.loginSuccess(response.data.user));
+        dispatch(modalSlice.actions.close())
+    } catch (e) {
+        if (e) {
+            dispatch(authSlice.actions.loginError(e.message))
+        }
+    }
+}
+
+export const registerFunc = (login, password) => async(dispatch) => {
+    try {
+        dispatch(authSlice.actions.login());
+        const response = await AuthService.register(login, password);
+        localStorage.setItem('token', response.data.accessToken);
+        dispatch(authSlice.actions.loginSuccess(response.data.user));
+    } catch (e) {
+        if (e) {
+            dispatch(authSlice.actions.loginError(e.message))
+        }
+    }
+}
+
+export const logoutFunc = () => async(dispatch) => {
+    try {
+        const response = await AuthService.logout();
+        localStorage.removeItem('token');
+        dispatch(authSlice.actions.logout())
+    } catch (e) {
+        if (e) {
+            dispatch(authSlice.actions.loginError(e.message))
+        }
+    }
+}
+
+export const checkAuth = () => async(dispatch) => {
+    try {
+        console.log(1)
+        const response = await axios.get(API_URL+"checkAuth");
+        dispatch(authSlice.actions.checkAuth({isAuth: true, user: response.data}))
+    } catch (e) {
+        if (e) {
+            dispatch(authSlice.actions.checkAuth({isAuth: false, user: {}}))
         }
     }
 }
---
+++ src/features/auth/Register.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled from 'styled-components'
+import useInput from '../../hooks/useInput'
+import { useDispatch } from 'react-redux'
+import { registerFunc } from '../../app/actionCreators'
+
+const AuthContainer = styled.div`
+    width: 100%;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    align-items: center;
+    justify-content: center;
+`
+
+
+const Authinput = styled.input`
+    background-color: #f7f7f7;
+    padding: 12px;
+    border-radius: 15px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`
+
+const AuthButton = styled.button`
+    padding: 20px;
+    background-color: #f7f7f7;
+    border-radius: 15px;
+    transition: all .35s ease-in;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+    &:hover {
+      background-color: #56195d;
+      color: white;
+    }
+    
+`
+
+const AuthText = styled.span`
+  font-size: 20px;
+  
+`
+
+const AuthBtn = styled.button`
+  color: #56195d;
+  font-weight: 500;
+  transition: all .35s ease-in;
+  &:hover {
+    color: #340e38;
+  }
+`
+
+function Register({setShowRegister}) {
+    const { value: login, onChange: handleLogin } = useInput()
+    const { value: password, onChange: handlePassword } = useInput()
+    const { value: password1, onChange: handlePassword1 } = useInput()
+    const { value: name, onChange: handleName } = useInput()
+    const dispatch = useDispatch();
+  
+    const handleSubmit = () => {
+      if (login.length > 3 && password.length > 7 && name.length > 3 && password1 === password) {
+        dispatch(registerFunc(login, password))
+      }
+    }
+
+  return (
+    <AuthContainer>
+      <Authinput 
+        placeholder='Логин'
+        value={login}
+        onChange={e => handleLogin(e.target.value)}
+      />
+      <Authinput 
+        placeholder='Пароль'
+        type='password'
+        value={password}
+        onChange={e => handlePassword(e.target.value)}
+      />
+    <Authinput 
+        placeholder='Повторите пароль'
+        type='password'
+        value={password1}
+        onChange={e => handlePassword1(e.target.value)}
+      />
+      <Authinput 
+        placeholder='Имя'
+        value={name}
+        onChange={e => handleName(e.target.value)}
+      />
+      <AuthButton onClick={handleSubmit} type='submit'>Зарегистрироваться</AuthButton>
+      <AuthText>Уже есть аккаунт? <AuthBtn onClick={() => setShowRegister(false)}>Войти!</AuthBtn></AuthText>
+    </AuthContainer>
+  )
+}
+
+export default Register
