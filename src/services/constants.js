56e37c878c6c188f58dd84fa8ad173baee30c25e
vagabund3219 <vagabund3219@gmail.com>
2023-09-25 20:00:12 +0300

    [ADD]: filters main page

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000/';
-export const API_URL = 'http://danilkaa-001-site1.ctempurl.com/';
+export const API_URL = 'https://danilkaa-001-site1.ctempurl.com/';
